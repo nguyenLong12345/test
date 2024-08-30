11c85b7a4636e20bce39379b4704fef4d357032c
nguyenLong12345 <22013896@st.phenikaa-uni.edu.vn>
2024-08-30 19:10:13 +0700

    netbean

---
+++ src/javaapplication9/ctestcn.java
@@ -0,0 +1,23 @@
+
+package javaapplication9;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class ctestcn {
+
+    public static void main(String[] args) {
+        String url = "jdbc:sqlserver://localhost:1433;databaseName=hoadon;encrypt=true;trustServerCertificate=true";
+
+        String user = "sa";
+        String password = "12345";
+
+        try {
+            Connection conn = DriverManager.getConnection(url, user, password);
+            System.out.println(conn.getCatalog());
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}

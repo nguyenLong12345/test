5612a453190a549b72df7346882d38c1637ec4ed
nguyenLong12345 <22013896@st.phenikaa-uni.edu.vn>
2024-08-30 19:43:07 +0700

    netconccc

--- src/javaapplication9/ctestcn.java
+++ src/javaapplication9/ctestcn.java
@@ -1,4 +1,3 @@
-
 package javaapplication9;
 
 import java.sql.Connection;
@@ -6,7 +5,6 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class ctestcn {
-
     public static void main(String[] args) {
         String url = "jdbc:sqlserver://localhost:1433;databaseName=hoadon;encrypt=true;trustServerCertificate=true";
 
